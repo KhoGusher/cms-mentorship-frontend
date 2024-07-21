0b8dc7c3ac461045f93107fa62d57809771ceb0d
Fraser Msusa <bete19-fmsusa@mubas.ac.mw>
2024-07-21 15:45:11 +0200

    Contactpage

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,42 +1,75 @@
-import config from "@/config/config.json";
-import { getListPage } from "@/lib/contentParser";
-import PageHeader from "@/partials/PageHeader";
-import SeoMeta from "@/partials/SeoMeta";
-import { RegularPage } from "@/types";
+"use client";
+import styles from "./contact.module.css";
 
-const Contact = async () => {
-  const data: RegularPage = getListPage("pages/contact.md");
-  const { frontmatter } = data;
-  const { title, description, meta_title, image } = frontmatter;
+import { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import { sendEmail } from '@/lib/utils/send-email';
+
+export type FormData = {
+  name: string;
+  email: string;
+  message: string;
+};
+
+
+
+const Contact: FC = () => {
+
+  const { register, handleSubmit } = useForm<FormData>();
+
+  function onSubmit(data: FormData) {
+    sendEmail(data);
+  }
 
   return (
     <>
-      <SeoMeta
-        title={title}
-        meta_title={meta_title}
-        description={description}
-        image={image}
-      />
-      <PageHeader title={title} />
-      <section className="section-sm">
-        <div className="container">
-          <div className="row">
-            <div className="mx-auto md:col-10 lg:col-6">
-              <p> 
-                <strong>Call Or Whatsapp :</strong> </p>
-                <p>+265 999 355 983</p> 
-                <strong>OR</strong> 
-                <p>+265 985 040 546</p> 
-                <strong>OR</strong>
-               <p> +265 991 254 738</p>
-              <br />
-              <p> <strong>Location:</strong> CHICHIRI BLANTYRE, MUBAS FLATS, CLOSE TO MRA </p>
-              
-            </div>
-          </div>
-        </div>
-      </section>
+   <section>
+<div>
+<div className="row">
+<div className={`mx-auto md:col-10 lg:col-6 flex flex-col justify-center  ${styles.row1}`}>
+<div className="relative m-7 p-4 h-auto" style={{ backgroundColor: 'rgba(207, 207, 215, 0.9)', borderRadius: '10px' }}>
+<h1 className="p-5 text-center ">Contact Us</h1>
+<p className="mb-3 text-left">Let's Talk</p>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className='mb-5'>
+        <input
+          type='text'
+          placeholder='Name'
+          className="form-control w-full bg-white bg-opacity-50 rounded-lg border border-gray-300"
+          {...register('name', { required: true })}
+        />
+      </div>
+      <div className='mb-5'>
+        <input
+          type='email'
+          placeholder='Email'
+          className="form-control w-full bg-white bg-opacity-50 rounded-lg border border-gray-300"
+          {...register('email', { required: true })}
+        />
+      </div>
+      <div className='mb-5'>        
+        <textarea
+          rows={4}
+          placeholder='Type your message'
+          
+          className="form-control w-full bg-white bg-opacity-50 rounded-lg border border-gray-300"
+          {...register('message', { required: true })}
+        ></textarea>
+      </div>
+      <div>
+        <button className="btn btn-primary bg-blue-950 mb-7">
+          Submit
+        </button>
+      </div>
+    </form>
+    </div>
+    </div>
+    </div>
+    </div>
+    </section>
     </>
+
+
   );
 };
 
--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -5,42 +5,77 @@ import { FaArrowRight } from "react-icons/fa";
 function News() {
     return (
         <div className="">
-            <div
-                className=" flex h-[40vh] items-center bg-cover bg-center bg-no-repeat px-[10vw]"
-                style={{ backgroundImage: `url('/images/news.jpg')` }}
-            >
-                <strong className="text-2xl text-black">NEWS</strong>
-            </div>
-            <div className="pt-[4vh] sm:px-[10vw]">
-                <div className="flex w-full flex-wrap gap-5">
-                    <NewsContainer />
-                    <NewsContainer />
-                    <NewsContainer />
-                    <NewsContainer />
-                </div>
-            </div>
+    <div
+        className="flex h-[20vh] items-end bg-cover bg-center bg-no-repeat px-[10vw] pl-7 pb-8"
+        style={{ backgroundImage: `url('/images/news.jpg')`}}
+    >
+        <strong className="text-3xl text-white">All News</strong>
+    </div>
+    <div className="pt-[4vh] sm:px-[10vw] flex flex-col md:flex-row lg:px-12 gap-10">
+        
+        <div className=" lg:w-3/5 flex flex-col gap-5 md:w-full">
+            <NewsContainer />
+            <NewsContainer />
+            <NewsContainer />
+            <NewsContainer />
         </div>
+
+        
+
+        <div className=" lg:w-2/5 p-4 md:w-full">
+        <div className="relative mb-4 w-full">
+    <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+    <svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 -960 960 960" width="24px" fill="grey"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
+    </span>
+    <input 
+        type="text" 
+        placeholder="Search news here..." 
+        className="w-full p-2 pl-10 border border-gray-300 rounded"
+    />
+</div>
+
+            <table className="w-full border border-collapse border-gray-400">
+                <tbody>
+                    <tr className="border">
+                        <td className="p-2 border">Recent News</td>
+                    </tr>
+                    <tr className="border">
+                        <td className="p-10 border">
+                        <NewsContainer />
+                        <NewsContainer />
+                        <div className="px-3 py-1 border-blue-600 border inline-block">
+                            View All
+                        </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
     );
 
     function NewsContainer() {
         return (
-            <div className="rounded-md shadow-md sm:w-[30%] mb-6">
-                <div className="h-[30vh] w-full rounded-t-md">
+            <div className="rounded-md shadow-md  mb-6 flex flex-row">
+                <div className="h-[30vh] w-full rounded-md flex-1 overflow-hidden">
                     <img
                         src="/images/news.jpg"
                         alt="News image"
-                        className="h-full w-full rounded-t-md object-cover"
+                        className="h-full w-full rounded-md object-cover"
                     />
                 </div>
                 
-                <div className="p-3">
+                <div className="p-3 flex-1">
                     <div className="w-full">
-                        <div className="flex w-full mb-3">
+                        {/* <div className="flex w-full mb-3">
                             <div className=" rounded-2xl bg-amber-500 py-1 px-2">
                                 6 days ago
                             </div>
-                        </div>
-                        <strong className="">
+                        </div> */}
+                        <strong className="text-blue-900" >
                             Gusherlabs launches an events app
                         </strong>
                         <p className="mt-3 text-gray-500">
@@ -48,7 +83,7 @@ function News() {
                             elit. Sed do eiusmod tempor incididunt ut labore et
                             dolore magna aliqua
                         </p>
-                        <Link href={`/news/1`} className="mt-3 flex gap-5">
+                        <Link href={`/news/1`} className="lg:mt-10 sm:mt-3 flex gap-5 text-blue-700 items-center">
                             read more{" "}
                             <span>
                                 <FaArrowRight />
