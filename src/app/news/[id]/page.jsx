fcd78072f65a0b5577c213cf303775589b900479
ishmaiah-ih <mwalwandaishmael@gmail.com>
2024-07-21 14:42:31 +0200

    Done with responsiveness

--- src/app/news/[id]/page.jsx
+++ src/app/news/[id]/page.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 function FullNewsPage() {
   return (
     <>
       {/* top banner */}
-
-
       <div
         className="flex h-[30vh] items-center bg-cover bg-center bg-no-repeat px-[10vw]"
         style={{ backgroundImage: `url('/images/ish-news-detail.webp')` }}
       >
-        <strong className="text-2xl text-white">NEWS DETAIL </strong>
+        <strong className="text-2xl text-white">NEWS DETAIL</strong>
       </div>
-      {/* end of top banner  */}
+      {/* end of top banner */}
 
-      <div className="grid lg:grid-cols-12 gap-10">
-        <div className="col-span-7 sm:mr-10">
-          <div className='pt-[4vh] sm:ml-[5vw]'>
-            <div className=' flex w-full flex-wrap gap-5'>
-              <div className="rounded-md shadow-sm sm:w-[100%]  mb-6">
+      <div className="grid lg:grid-cols-12 gap-10 p-4">
+        <div className="col-span-12 lg:col-span-7 sm:mr-10">
+          <div className='pt-[4vh] lg:ml-[5vw]'>
+            <div className='flex w-full flex-wrap gap-5'>
+              <div className="rounded-md shadow-sm w-full mb-6">
                 <div className="h-[50vh] w-full rounded-t-md">
                   <img
                     src="/images/news.jpg"
@@ -29,78 +27,52 @@ function FullNewsPage() {
 
                 <div className="p-3">
                   <div className="w-full">
-                    {/* <div className="flex w-full mb-3">
-                  <div className=" rounded-2xl bg-amber-500 py-1 px-2">
-                    6 days ago
-                  </div>
-                </div> */}
                     <strong className="mt-3">
                       Gusherlabs launches an events app
                     </strong>
                     <p className="mt-3 text-gray-500">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing
-                      elit. Sed do eiusmod tempor incididunt ut labore et
-                      dolore magna aliqua <br/><br/>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing
-                      elit. Sed do eiusmod tempor incididunt ut labore et
-                      dolore magna aliqua<br/><br/>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing
-                      elit. Sed do eiusmod tempor incididunt ut labore et
-                      dolore magna aliqua<br/><br/>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing
-                      elit. Sed do eiusmod tempor incididunt ut labore et
-                      dolore magna aliqua<br/><br/>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing
-                      elit. Sed do eiusmod tempor incididunt ut labore et
-                      dolore magna aliqua<br/><br/>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing
-                      elit. Sed do eiusmod tempor incididunt ut labore et
-                      dolore magna aliqua<br/><br/>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing
-                      elit. Sed do eiusmod tempor incididunt ut labore et
-                      dolore magna aliqua<br/><br/>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing
-                      elit. Sed do eiusmod tempor incididunt ut labore et
-                      dolore magna aliqua
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua <br/><br/>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua<br/><br/>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua<br/><br/>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua<br/><br/>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua<br/><br/>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua<br/><br/>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua<br/><br/>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
                     </p>
-
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="md:col-span-4   bg-white border border-gray-500  p-4 sm:h-[100vh] h-auto   pt-[4vh] mt-6 rounded-md">
+        <div className="col-span-12 lg:col-span-4 bg-white border border-gray-500 p-4 h-auto lg:h-[100vh] pt-[4vh] mt-6 rounded-md">
           <div>
             <form className='text-center'>
               <input className='w-full rounded-sm' type='text' placeholder='search news here...' />
             </form>
           </div>
-          <div className='text-gray-500 mb-5 mt-2  text-center'>RECENT NEWS</div>
+          <div className='text-gray-500 mb-5 mt-2 text-center'>RECENT NEWS</div>
           <hr className='border mt-3 border-gray-500'/>
           <div className='w-full mt-6'>
-            <div className=" ">
+            <div>
               <NewsContainer />
               <NewsContainer />
-              
             </div>
             <div>
-            <button className='mt-3 hover:bg-gray-500 hover:border-none hover:text-white btn rounded-none border-blue-500  text-primary'>view all</button>
-          </div>
+              <button className='mt-1 hover:bg-gray-500 hover:border-none hover:text-white btn rounded-none border-blue-500 text-primary'>view all</button>
+            </div>
           </div>
         </div>
       </div>
-
     </>
-
-  )
+  );
 }
-
 
 function NewsContainer() {
   return (
-    <div className="rounded-sm flex ">
-      <div className="w-full  rounded-md mb-5">
+    <div className="rounded-sm flex mb-3">
+      <div className="w-full rounded-md">
         <img
           src="/images/mus.webp"
           alt="News image"
@@ -110,20 +82,14 @@ function NewsContainer() {
 
       <div className="p-3">
         <div className="w-full">
-
-          <strong className="">
-            Gusherlabs launches an events app
-          </strong>
+          <strong>Gusherlabs launches an events app</strong>
           <p className="mt-5 text-gray-500">
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua...
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...
           </p>
-          
         </div>
       </div>
     </div>
   );
 }
 
-export default FullNewsPage
+export default FullNewsPage;
--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -4,18 +4,18 @@ import { FaArrowRight } from "react-icons/fa";
 
 function News() {
     return (
-        <div className="">
+        <div>
             <div
-                className=" flex h-[35vh] items-center bg-cover bg-center bg-no-repeat px-[10vw]"
+                className="flex h-[35vh] items-center bg-cover bg-center bg-no-repeat px-[10vw]"
                 style={{ backgroundImage: `url('/images/ish-news.jpg')` }}
             >
                 <strong className="text-2xl text-white font-extrabold">ALL NEWS </strong>
             </div>
 
-            <div className="grid grid-cols-12 gap-4">
-                <div className="col-span-8">
-                    <div className="pt-[4vh] sm:px-[10vw]">
-                        <div className=" ">
+            <div className="grid grid-cols-12 gap-4 p-4">
+                <div className="col-span-12 lg:col-span-8">
+                    <div className="pt-[4vh] lg:px-[10vw]">
+                        <div>
                             <NewsContainer />
                             <NewsContainer />
                             <NewsContainer />
@@ -23,22 +23,21 @@ function News() {
                         </div>
                     </div>
                 </div>
-                <div className="col-span-4 bg-white border border-gray-500  p-4 h-[100vh] mr-5 pt-[4vh] mt-6 rounded-md">
+                <div className="col-span-12 lg:col-span-4 bg-white border border-gray-500 p-4 h-auto lg:h-[100vh] lg:mr-5 pt-[4vh] mt-6 rounded-md">
                     <div>
                         <form className='text-center'>
                             <input className='w-full rounded-sm' type='text' placeholder='search news here...' />
                         </form>
                     </div>
-                    <div className='text-gray-500 mb-5 mt-2  text-center'>RECENT NEWS</div>
+                    <div className='text-gray-500 mb-5 mt-2 text-center'>RECENT NEWS</div>
                     <hr className='border mt-3 border-gray-500' />
                     <div className='w-full mt-6'>
-                        <div className=" ">
+                        <div>
                             <NewsCont />
                             <NewsCont />
-
                         </div>
                         <div>
-                            <button className='mt-3 hover:bg-gray-500 hover:border-none hover:text-white btn rounded-none border-blue-500  text-primary'>view all</button>
+                            <button className='mt-1 hover:bg-gray-500 hover:border-none hover:text-white btn rounded-none border-blue-500 text-primary'>view all</button>
                         </div>
                     </div>
                 </div>
@@ -48,36 +47,30 @@ function News() {
 
     function NewsCont() {
         return (
-          <div className="rounded-sm flex ">
-            <div className="w-full  rounded-md mb-5">
-              <img
-                src="/images/mus.webp"
-                alt="News image"
-                className="h-full w-full rounded-tl-md rounded-bl-md object-cover"
-              />
+            <div className="rounded-sm flex mb-3">
+                <div className="w-full rounded-md">
+                    <img
+                        src="/images/mus.webp"
+                        alt="News image"
+                        className="h-full w-full rounded-tl-md rounded-bl-md object-cover"
+                    />
+                </div>
+
+                <div className="p-3">
+                    <div className="w-full">
+                        <strong>Gusherlabs launches an events app</strong>
+                        <p className="mt-5 text-gray-500">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...
+                        </p>
+                    </div>
+                </div>
             </div>
-      
-            <div className="p-3">
-              <div className="w-full">
-      
-                <strong className="">
-                  Gusherlabs launches an events app
-                </strong>
-                <p className="mt-5 text-gray-500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing
-                  elit. Sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua...
-                </p>
-                
-              </div>
-            </div>
-          </div>
         );
-      }
+    }
 
     function NewsContainer() {
         return (
-            <div className="rounded-md shadow-md  mb-6 flex hover:shadow-inner">
+            <div className="rounded-md shadow-md mb-6 flex hover:shadow-inner">
                 <div className="h-[35vh] w-full rounded-md">
                     <img
                         src="/images/news.jpg"
@@ -88,24 +81,19 @@ function News() {
 
                 <div className="p-3">
                     <div className="w-full">
-
-                        <strong className="">
-                            Gusherlabs launches an events app
-                        </strong>
+                        <strong>Gusherlabs launches an events app</strong>
                         <p className="mt-3 text-gray-500">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua...
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...
                         </p>
                         <div className="flex justify-around items-center mt-3">
-                            <Link href={`/news/1`} className=" flex items-center gap-1 text-blue-700">
+                            <Link href={`/news/1`} className="flex items-center gap-1 text-blue-700">
                                 read more{" "}
                                 <span>
                                     <FaArrowRight />
                                 </span>
                             </Link>
-                            <div className="flex  mb-3">
-                                <div className=" rounded-2xl bg-amber-500 py-1 px-2 ">
+                            <div className="flex mb-3">
+                                <div className="rounded-2xl bg-amber-500 py-1 px-2">
                                     6 days ago
                                 </div>
                             </div>
